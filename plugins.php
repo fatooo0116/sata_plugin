4833e7e4c0b1c23760159f4dba222e6208ef991e
fatooo0116 <fatooo0116@gmail.com>
2022-01-21 15:37:27 +0800

    i

---
+++ plugins.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Aloha Eetension
+Plugin URI: https://aloha-tech.com/
+Description: 增加會員欄位，增加訂單查詢介面
+Author: Mike Hsu
+Version: 1.0.1
+Author URI: https://aloha-tech.com/
+*/
+
+
+
+/*  Add field to checkout  */
+
+/* Add field to member page */
+
+/* Add  field to Wp User page */
+
+include "option_order.php";
